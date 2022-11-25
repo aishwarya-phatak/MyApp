dff204c93d8d0d88fb0d1fbbbc4c9e2a89d45dd5
aishwarya <ashp17star@gmail.com>
2022-11-25 18:20:47 +0530

    Updated

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtvw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
